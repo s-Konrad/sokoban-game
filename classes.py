5bbbf29b0482594a2bc0a1e55777190e4fc80733
Konrad <kskwierczynskii@gmail.com>
2024-12-29 15:24:57 +0100

    translated game.py funcionality to Game class in classes.py

--- classes.py
+++ classes.py
@@ -6,6 +6,22 @@ class OcupationError(Exception):
     pass
 
 
+class UnableToHold(Exception):
+    pass
+
+
+class InvalidCoordinates(ValueError):
+    pass
+
+
+class HasBox(Exception):
+    pass
+
+
+class NotStepable(Exception):
+    pass
+
+
 # @dataclass
 class Tile():
     '''
@@ -16,22 +32,18 @@ class Tile():
     3 button
     # 4 box spawn inherit tile_stepable tile(tile), Inaccesive_tile
     # 5 player spawn to na @TODO no spawn tiles only floro
-    jak najmniej if można też zrobić inaccesibleTile po prostu w tile skoro i 
-    tak nic nie robi
+    jak najmniej if można też zrobić inaccesibleTile po prostu w tile skoro i
+    tak nic nie robi zrobic przesuwanie z try
     '''
-    def __init__(self,
-                 coordinates,
-                 tile_type='void',) -> None:
+    def __init__(self, coordinates) -> None:
 
         self.__location = coordinates
-        try:
-            self.__type = tile_type
-        except Exception:
-            raise Exception
 
         if not (0 <= coordinates[0] < 32 and 0 <= coordinates[1] < 32):
-            raise ValueError
+            raise InvalidCoordinates
 
+    def is_stepable(self):
+        return False
         # if tile_type in [0, 1]:
         #     if ocupied is not None:
         #         raise InvalidTile
@@ -43,52 +55,43 @@ class Tile():
 
 
 class StepableTile(Tile):
-    def __init__(self, coordinates, tile_type='floor', ocupied=None):
-        super().__init__(coordinates, tile_type)
-        self.__ocupied = ocupied
+    def __init__(self, coordinates, ocupied=False):
+        super().__init__(coordinates)
+        self._ocupied = ocupied
+
+# negate stepable from notStepable Class
+    def can_hold(self):
+        return not self._ocupied
 
     def set_ocupation(self, oc):
         # if?
-        self.__ocupied = oc
-    
+        self._ocupied = oc
+
     def __str__(self):
         return 'floor'
 
 
 class ButtonTile(StepableTile):
-    def __init__(self, coordinates, tile_type='button', ocupied=None):
-        super().__init__(coordinates, tile_type, ocupied)
+    def __init__(self, coordinates, ocupied=False):
+        super().__init__(coordinates, ocupied)
         self._ocupied = ocupied
 
     def is_pressed(self):
-        return self._ocupied == 'box'
+        return self._ocupied
 
     def __str__(self):
         return 'button'
 
 
 class InaccesibleTile(Tile):
-    def __init__(self, coordinates, tile_type='void'):
-        super().__init__(coordinates, tile_type)
-
-# class WallTile(Tile):
-#     def __init__(self, coordinates, has_box=None):
-#         super().__init__(coordinates, has_box=None)
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
 
 
-# class FloorTile(Tile):
-#     def __init__(self, coordinates, has_box=None):
-#         super().__init__(coordinates, has_box=None)
+# class WallTile(Tile): ->> inaccesibleTile
+# odpowiednie metody i hierarchia
 
-
-#     def is_taken(self):
-#         return self.__has_box
-
-
-# class ButtonTile(Tile):
-#     def __init__(self, coordinates, has_box=None):
-#         super().__init__(coordinates, has_box=False)
-class Player():
+class Agent():
     def __init__(self, coordinates):
         self.__location_x, self.__location_y = coordinates
 #  z zewnątrz sprawdzanie czy jest możliwość ruchu
@@ -109,21 +112,128 @@ class Player():
         self.__location_x += 1
 
 
-class Box():
-    def __init__(self, coordinates):
-        self.__location_x, self.__location_y = coordinates
+class Game():
+    def __init__(self, player, level, boxes):
+        self.load_level(player, level, boxes)
 
-    def pos(self):
-        return (self.__location_x, self.__location_y)
+    def load_level(self, player, level, boxes):
+        self._player = player
+        self._level, self._title, self._buttons = level
+        self._boxes = boxes
+        self._moves = []
+        for box_id in self._boxes:
+            self._level[self._boxes[box_id].pos()].set_ocupation(True)
 
-    def push_up(self):
-        pass
+    @property
+    def player(self):
+        return self._player
 
-    def push_down(self):
-        pass
+    @property
+    def title(self):
+        return self._title
 
-    def push_left(self):
-        pass
+    @property
+    def level(self):
+        return self._level
 
-    def push_right(self):
-        pass
+    @property
+    def boxes(self):
+        return self._boxes
+
+    @property
+    def moves(self):
+        return len(self._moves)
+
+    def finb_box(self, coordinates):
+        for box_id in self._boxes:
+            if self._boxes[box_id].pos() == coordinates:
+                return box_id
+        else:
+            raise IndexError
+    # rozdzielic move na wykonywanie ruchu i sprawdzanie warunku.
+
+    def validate_move(self, move):
+        try:
+            self._moves.append(move)
+            self.move(move, self._player)
+
+        except KeyError:
+            self.undo_move()
+            return
+        except AttributeError:
+            self.undo_move()
+            return
+        try:
+            box = self._boxes[self.finb_box(self.player.pos())]
+            try:
+                self.move(move, box)
+                if not self._level[box.pos()].can_hold():
+                    self.undo_move(box)
+                    return
+
+                self._level[self.player.pos()].set_ocupation(False)
+                self._level[box.pos()].set_ocupation(True)
+
+                # print(box.pos())
+            except KeyError:
+                self.undo_move(box)
+                return
+            except AttributeError:
+                self.undo_move(box)
+                return
+        except IndexError:
+            pass
+
+    def move(self, move, agent):
+        actions = {
+            'w': Agent.move_up,
+            'a': Agent.move_left,
+            's': Agent.move_down,
+            'd': Agent.move_right,
+        }
+        action = actions.get(move)
+        # player = self.player
+        level = self.level
+
+        action(agent)
+
+        tile = level[agent.pos()]
+        tile.can_hold()
+        # try:
+        #     box = self.finb_box(player.pos())
+        #     try:
+        #         action(box)
+        #     except Ellipsis:
+        #         pass
+        #     # self.undo_move()
+        # except IndexError:
+        #     pass
+
+
+        # zamienić to gorne na sprawdzanie wywolania tile.is stepable
+        # for box_id in self._boxes:
+        #     if self._boxes[box_id].pos() == self._player.pos():
+        #         action.get(move)(self._boxes[box_id])
+        #         if
+        #         break
+
+    def undo_move(self, agent=None):
+        reverse_action = {
+            'w': Agent.move_down,
+            'a': Agent.move_right,
+            's': Agent.move_up,
+            'd': Agent.move_left,
+        }
+        reverse_action.get(self._moves[-1])(self._player)
+        if agent is not None:
+            reverse_action.get(self._moves[-1])(agent)
+
+        self._moves.pop()
+# zmienic values na liste
+    def game_ended(self):
+        for button_id in self._buttons:
+            if not self._level[button_id].is_pressed():
+                return False
+        else:
+            return True
+
--- game.py
+++ game.py
@@ -1,49 +1,39 @@
-from level import get_level, get_player, get_boxes
+from level import get_floor, get_player, get_boxes
 from PySide6.QtCore import Qt
-import copy
-from classes import Player
-# from gui import Gui, gui_main
 
-def render(level, agents):
-    p = 0
-    s = ''
-    for tile in level:
-        if tile[1] == p:
-            s += str(level[tile])
-        else:
-            p += 1
-            print(s)
-            s = str(level[tile])
-    print(s)
-    # for agent in agents:
-        # print(agent, ' - ', agents[agent].pos())
+from classes import Agent, Game
+from gui import Gui, gui_main
+
+
 # @TODO przenieść gui i sprawdzanie prezsuwania tutaj
 # sprawdzanie try except box in the way
 # z classes moveup tu z tad do gui może jako jedno move?
 
 
-def move(player, boxes, level, event):
-    action = {
-        Qt.Key_W: Player.move_up,
-        Qt.Key_S: Player.move_down,
-        Qt.Key_A: Player.move_left,
-        Qt.Key_D: Player.move_right,
-    }
-    action.get(event.key())(player)
-    if player.pos() not in level:
-        undo_move(player, event)
+# def move(player, boxes, level, event):
+#     action = {
+#         Qt.Key_W: Agent.move_up,
+#         Qt.Key_S: Agent.move_down,
+#         Qt.Key_A: Agent.move_left,
+#         Qt.Key_D: Agent.move_right,
+#     }
+#     action.get(event.key())(player)
+#     if player.pos() not in level:
+#         undo_move(player, event)
         # print(player.pos())
 
 
-# usunac te wszystkie przekazywania bo i tak to bedzie wykonywane z poziomu game gdy zwine to z gui
+# usunac te wszystkie przekazywania bo i tak to bedzie wykonywane z poziomu
+# game gdy zwine to z gui
 def undo_move(player, event):
-    reverse_action = {
-        Qt.Key_S: Player.move_up,
-        Qt.Key_W: Player.move_down,
-        Qt.Key_D: Player.move_left,
-        Qt.Key_A: Player.move_right,
-    }
-    reverse_action.get(event.key())(player)
+    pass
+    # reverse_action = {
+    #     Qt.Key_S: Agent.move_up,
+    #     Qt.Key_W: Agent.move_down,
+    #     Qt.Key_D: Agent.move_left,
+    #     Qt.Key_A: Agent.move_right,
+    # }
+    # reverse_action.get(event.key())(player)
 
 # def move_up(player, boxes):
 #     player.move_up()
@@ -54,39 +44,14 @@ def undo_move(player, event):
 #             print('ns', boxes[box_id].pos())
 
 
-# def move_down(player, boxes):
-#     try:
-#         player.move_down()
-#         print(player.pos())
-#     except boxes[player.pos()]:
-#         boxes[player.pos()].push_down()
-
-
-# def move_left(player, boxes):
-#     try:
-#         player.move_left()
-#         print(player.pos())
-#     except boxes[player.pos()]:
-#         boxes[player.pos()].push_left()
-
-
-# przeniesc player i box do jednej klasy z move
-# def move_right(player, boxes):
-#     try:
-#         player.move_right()
-#         # print(player.pos())
-#     except boxes[player.pos()]:
-#         boxes[player.pos()].push_right()
-
-
 # zamieniać wywoływanie gui zeby wychodziło z game i zeby level było dostepne
 # wtedy ruchy bylyby dostęne do sprawdzania
 def game():
     # read tiles placement and its contents from file
     moves = []
-    level, agents = get_level()
-    render(level, agents)
-    # gui_main()
+    # level, agents = get_floor()
+
+    gui_main()
 
 
 if __name__ == '__main__':
