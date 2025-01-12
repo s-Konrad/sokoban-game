1946c287d604d373776763c3f59b21ba4ba32f10
Konrad <kskwierczynskii@gmail.com>
2025-01-12 17:14:30 +0100

    added test for convert to int function

--- tests/test_level.py
+++ tests/test_level.py
@@ -1,6 +1,14 @@
 import pytest
 from model import InvalidTile, InvalidAgent
-from load_level import get_title, get_board, get_player, get_boxes
+from load_level import (get_title, get_board, get_player, get_boxes,
+                        convert_to_int)
+
+
+def test_convert_to_int():
+    assert convert_to_int([1, '1']) == (1, 1)
+    with pytest.raises(ValueError):
+        convert_to_int([1, 'da'])
+
 
 SAMPLE_DATA = {
     'title': 'Test Level',
@@ -57,7 +65,7 @@ def test_duplicate_tile():
         get_board(duplicate_tile_data)
 
 
-def test_incorect_tile_type():
+def test_invalid_tile_type():
     duplicate_tile_data = {
         'title': 'Test Level with Duplicate Tiles',
         'tiles': [
@@ -70,6 +78,21 @@ def test_incorect_tile_type():
 
     with pytest.raises(InvalidTile):
         get_board(duplicate_tile_data)
+
+
+def test_invalid_tile_location():
+    invalid_tile_data = {
+        'title': 'Test Level with Duplicate Tiles',
+        'tiles': [
+            {'id': ['ds', 0], 'type': 'floor'},
+            {'id': [0, 1], 'type': 'unknown'}
+        ],
+        'player_pos': [0, 0],
+        'boxes': []
+    }
+
+    with pytest.raises(ValueError):
+        get_board(invalid_tile_data)
 
 
 def test_duplicate_box_id():
@@ -85,3 +108,48 @@ def test_duplicate_box_id():
 
     with pytest.raises(InvalidAgent):
         get_boxes(duplicate_box_data)
+
+
+def test_invalid_box_id():
+    invalid_box_data = {
+        'title': 'Test Level with Duplicate Boxes',
+        'tiles': [{'id': [0, 0], 'type': 'floor'}],
+        'player_pos': [0, 0],
+        'boxes': [
+            {'id': 'ds', 'position': [1, 1]},
+            {'id': 1, 'position': [2, 1]}
+        ]
+    }
+
+    with pytest.raises(ValueError):
+        get_boxes(invalid_box_data)
+
+
+def test_invalid_box_pos():
+    invalid_box_data = {
+        'title': 'Test Level with Duplicate Boxes',
+        'tiles': [{'id': [0, 0], 'type': 'floor'}],
+        'player_pos': [0, 0],
+        'boxes': [
+            {'id': 0, 'position': ['$$$', 1]},
+            {'id': 1, 'position': [2, 1]}
+        ]
+    }
+
+    with pytest.raises(ValueError):
+        get_boxes(invalid_box_data)
+
+
+def test_invalid_player_pos():
+    invalid_box_data = {
+        'title': 'Test Level with Duplicate Boxes',
+        'tiles': [{'id': [0, 0], 'type': 'floor'}],
+        'player_pos': [0, '$k$'],
+        'boxes': [
+            {'id': 0, 'position': [1, 1]},
+            {'id': 1, 'position': [2, 1]}
+        ]
+    }
+
+    with pytest.raises(ValueError):
+        get_player(invalid_box_data)
