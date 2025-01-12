92015883a540fe902c353700bec52eb0cd6ee1ad
Konrad <kskwierczynskii@gmail.com>
2025-01-12 18:51:50 +0100

    fixesd typo in 'Sokoban' all across the project

--- headers/ui_gui.py
+++ headers/ui_gui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'sokobon.ui'
+## Form generated from reading UI file 'sokoban.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
